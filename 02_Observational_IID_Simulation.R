b6717c4a974c700f3e0cc5989a75a853046b26b0
owen-hy <owenhy@umich.edu>
2025-06-24 13:04:45 -0400

    Change to proper IID structure

--- 02_Observational_IID_Simulation.R
+++ 02_Observational_IID_Simulation.R
@@ -1,5 +1,6 @@
 # File Purpose: This R script is designed to account for an observational setting
 # Specifically, we are now dealing with a scenario where {Y_ij(0), Y_ij(1)} _|_ T_ij | X_ij
+# This scenario is for when we assume that individuals are IID
 
 library(tidyverse)
 library(MASS)
@@ -78,18 +79,14 @@ calculate_ATE_prop <- function(data, parametric){
 boot_ATE <- function(data, parametric){
   sample_ATE <- replicate(200, {
     # Should this be by individual or by clusters? Seemingly Clusters
-    sample_idx <- sample(1:length(unique(data$cluster_num)), size = 30 ,replace = T)
-    boot_data <- lapply(seq_along(sample_idx), function(idx){
-      data[data$cluster_num == sample_idx[idx], ] |>
-        mutate(cluster_num = idx)
-    })
-    boot_data <- data.table::rbindlist(boot_data)
+    sample_idx <- sample(1:nrow(data), size = 1000 ,replace = T)
+    boot_data <- data[sample_idx, ]
     calculate_ATE_prop(boot_data, parametric)
   })
   return(c(quantile(sample_ATE, probs = 0.025, na.rm = T), quantile(sample_ATE, probs = 0.975, na.rm = T)))
 }
 
-ATE_sim_one_obs <- function(cluster_range, parametric, ICC, independent){
+ATE_sim_one_obs <- function(cluster_range, parametric,independent){
   ATE <- rep(NA, num_iter)
   ATE_est <- rep(NA, num_iter)
   coverage <- rep(NA, num_iter)
@@ -99,41 +96,32 @@ ATE_sim_one_obs <- function(cluster_range, parametric, ICC, independent){
       # Generating our covariate matrix X_i per cluster
       cluster_size <- rdunif(1, cluster_range[1], cluster_range[2])
       x1 <- rnorm(cluster_size, mean = 1, sd = 1)
-      x2 <- mvrnorm(mu = rep(0.5, cluster_size), Sigma = ((0.5 * diag(x = cluster_size)) + (
-        0.05 * matrix(
-          data = 1,
-          nrow = cluster_size,
-          ncol = cluster_size
-        )
-      )))
+      x2 <- rnorm(cluster_size, mean = 0.5, sd = 1)
       x3 <- rbinom(cluster_size, 1, 0.55)
-      x4 <- rep(runif(1), cluster_size)
+      x4 <- runif(cluster_size)
       cluster_level_data <- rbind(x1, x2, x3, x4) #X_i
       # Generating our observed outcome, Y_ij, Y*_ij, and V_ij
       # Starting with Y_ij(0) and Y_ij(1)
-      b_i_y <- rnorm(1, 0, sqrt((ICC * (pi^2)) / (3 * (1 - ICC))))
-      Y_ij_0_pi <- inv.logit((-1) + (c(0.15, 0.2, 0.15, -0.15) %*% cluster_level_data) + b_i_y) # Bernoulli parameter for calculating Y_ij(0)
+      Y_ij_0_pi <- inv.logit((-1) + (c(0.15, 0.2, 0.15, -0.15) %*% cluster_level_data)) # Bernoulli parameter for calculating Y_ij(0)
       Y_ij_0 <- rbern(length(Y_ij_0_pi), Y_ij_0_pi)
-      Y_ij_1_pi <- inv.logit((-0.25) + (c(0.15, 0.2, 0.15, -0.15) %*% cluster_level_data) + b_i_y) # Bernoulli parameter for calculating Y_ij(1)
+      Y_ij_1_pi <- inv.logit((-0.25) + (c(0.15, 0.2, 0.15, -0.15) %*% cluster_level_data)) # Bernoulli parameter for calculating Y_ij(1)
       Y_ij_1 <- rbern(length(Y_ij_1_pi), Y_ij_1_pi)
       # Now moving onto Y*_ij(0) and Y*_ij(1)
-      b_i_ystar <- 0 # READ: Paper stated that they kept this at zero?
       if(independent){
-        Y_ij_0_star_pi <-  inv.logit(-1.25 + (1.5 * Y_ij_0) + b_i_ystar)# Bernoulli parameter for calculating Y*_ij(0)
+        Y_ij_0_star_pi <-  inv.logit(-1.25 + (1.5 * Y_ij_0))# Bernoulli parameter for calculating Y*_ij(0)
         Y_ij_star_0 <- rbern(length(Y_ij_0_star_pi), Y_ij_0_star_pi)
-        Y_ij_1_star_pi <-  inv.logit(-1 + (2.5 * Y_ij_1) + b_i_ystar) # Bernoulli parameter for calculating Y*_ij(1)
+        Y_ij_1_star_pi <-  inv.logit(-1 + (2.5 * Y_ij_1)) # Bernoulli parameter for calculating Y*_ij(1)
         Y_ij_star_1 <- rbern(length(Y_ij_1_star_pi), Y_ij_1_star_pi)
       } else{
-        Y_ij_0_star_pi <-  inv.logit(-1.75 + (c(0.25, -0.25, -0.15, -0.1) %*% cluster_level_data) + (1.5 * Y_ij_0) + b_i_ystar)# Bernoulli parameter for calculating Y*_ij(0)
+        Y_ij_0_star_pi <-  inv.logit(-1.75 + (c(0.25, -0.25, -0.15, -0.1) %*% cluster_level_data) + (1.5 * Y_ij_0))# Bernoulli parameter for calculating Y*_ij(0)
         Y_ij_star_0 <- rbern(length(Y_ij_0_star_pi), Y_ij_0_star_pi)
-        Y_ij_1_star_pi <-  inv.logit(-1.25 + (c(-0.25, -0.15, -0.25, -0.1) %*% cluster_level_data) + (2.5 * Y_ij_1) + b_i_ystar) # Bernoulli parameter for calculating Y*_ij(1)
+        Y_ij_1_star_pi <-  inv.logit(-1.25 + (c(-0.25, -0.15, -0.25, -0.1) %*% cluster_level_data) + (2.5 * Y_ij_1)) # Bernoulli parameter for calculating Y*_ij(1)
         Y_ij_star_1 <- rbern(length(Y_ij_1_star_pi), Y_ij_1_star_pi)
       }
       # Now, we calculate V_ij
-      b_iv <- rnorm(1, 0, sqrt((ICC * (pi^2)) / (3 * (1 - ICC))))
-      V_ij_0_pi <- inv.logit((-0.25) + (c(-0.5, -0.5, 0.25, -0.25) %*% cluster_level_data) + (-0.15 * Y_ij_0)  + b_iv) # Bernoulli parameter for calculating V_ij(0)
+      V_ij_0_pi <- inv.logit((-0.25) + (c(-0.5, -0.5, 0.25, -0.25) %*% cluster_level_data) + (-0.15 * Y_ij_0)) # Bernoulli parameter for calculating V_ij(0)
       V_ij_0 <- rbern(length(V_ij_0_pi), V_ij_0_pi)
-      V_ij_1_pi <- inv.logit((-0.5) + (c(-0.5, -0.5, 0.25, -0.25) %*% cluster_level_data) + (0.15 * Y_ij_1)  + b_iv)  # Bernoulli parameter for calculating V_ij(1)
+      V_ij_1_pi <- inv.logit((-0.5) + (c(-0.5, -0.5, 0.25, -0.25) %*% cluster_level_data) + (0.15 * Y_ij_1))  # Bernoulli parameter for calculating V_ij(1)
       V_ij_1 <- rbern(length(V_ij_1_pi), V_ij_1_pi)
       # Finally, We randomly assign our treatment (READ: this is the biggest difference between the first file and this one
       # assume IID for now, even if this logically doesn't make sense in a cluster based setting)
@@ -181,20 +169,18 @@ ATE_sim_one_obs <- function(cluster_range, parametric, ICC, independent){
               bias_se = sqrt(var(ATE_est) / num_iter),
               cov_se = sqrt((mean(coverage) * (1 - mean(coverage))) / num_iter),
               size = if_else(cluster_range[1] == 100, "small", "large"),
-              parametric = parametric, ICC = ICC, independent = independent))
+              parametric = parametric, independent = independent))
 }
 
 # Monte Carlo simulation per model
 
 small <- c(100, 300)
 large <- c(500, 1000)
-ICC_vals <- c(0.01, 0.1)
 size_range <- list(small, large)
 
 parameters <- expand.grid(
   size_idx = 1:2,
   para = c(F, T),
-  ICC = ICC_vals,
   ind = c(F, T),
   stringsAsFactors = FALSE
 )
@@ -206,7 +192,7 @@ n_jobs <- nrow(parameters)
 
 system.time(results <- mclapply(1:n_jobs, function(i){
   param <- parameters[i, ]
-  ATE_sim_one_obs(size_range[[param$size_idx]], param$para, param$ICC, param$ind)
+  ATE_sim_one_obs(size_range[[param$size_idx]], param$para, param$ind)
 }, mc.cores = 50)
 )
 
